8f04d145849ce5a25e2d429a84996318b02dad86
John M Duffy <johnduffynh@users.noreply.github.com>
2021-07-06 05:43:59 -0400

    Fix build.

--- source/GoogleDrive.h
+++ source/GoogleDrive.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "./Exports.h"
 #include "../../Framework/source/io/DiskWatcher.h"
-//#include "io/drive/DriveApi.h"
 
 extern "C" JDE_GOOGLE_EXPORT Jde::IO::IDrive* LoadDrive()noexcept;
 
@@ -9,15 +8,14 @@ namespace Jde::IO::Drive
 {
 	struct GoogleDrive final:	public IDrive
 	{
-		GoogleDrive()noexcept{ DBG0("GoogleDrive::GoogleDrive"sv); };
-		~GoogleDrive(){ DBG0("GoogleDrive::~GoogleDrive"sv); }
+		GoogleDrive()noexcept{ DBG("GoogleDrive::GoogleDrive"sv); };
+		~GoogleDrive(){ DBG("GoogleDrive::~GoogleDrive"sv); }
 		map<string,IDirEntryPtr> Recursive( path dir )noexcept(false) override;
 		IDirEntryPtr Get( path path )noexcept(false) override{ THROW( Exception("Not Implemented") ); }
 		void SoftLink( path existingFile, path newSymLink )noexcept(false) override{ THROW( Exception("Not Implemented") ); }
 		IDirEntryPtr Save( path path, const vector<char>& bytes, const IDirEntry& dirEntry )noexcept(false) override{ return Save( path, bytes, dirEntry, 0 ); }
 		IDirEntryPtr Save( path path, const vector<char>& bytes, const IDirEntry& dirEntry, uint retry )noexcept(false);
 		IDirEntryPtr CreateFolder( path path, const IDirEntry& dirEntry )noexcept(false) override;
-		//VectorPtr<char> Load( path path )noexcept(false) override;
 		VectorPtr<char> Load( const IDirEntry& dirEntry )noexcept(false) override;
 		void Remove( path path )noexcept(false) override;
 		void Trash( path path )noexcept(false) override;
