59e5050252a092dd7995ad7a4cbd5bb3b63a596c
jde-cpp <johnduffyx@outlook.com>
2025-02-14 16:16:22 -0500

    move to boost json

--- source/TokenInfo.h
+++ source/TokenInfo.h
@@ -1,10 +1,11 @@
 #pragma once
 
-namespace Jde::Google
-{
+namespace Jde::Google{
+
 	//result from https://oauth2.googleapis.com/tokeninfo?id_token=
-	struct TokenInfo
-	{
+	struct TokenInfo{
+		TokenInfo()=default;
+		TokenInfo( const jobject& j )ι;
 		string Iss;
 		string Azp;
 		string Aud;
@@ -18,33 +19,18 @@ namespace Jde::Google
 		time_t Iat{};
 		time_t Expiration{};
 	};
-	inline void from_json( const nlohmann::json& j, TokenInfo& token )noexcept(false)
-	{
-		if( j.find("iss")!=j.end() )
-			j.at("iss").get_to( token.Iss );
-		if( j.find("azp")!=j.end() )
-			j.at("azp").get_to( token.Azp );
-		if( j.find("aud")!=j.end() )
-			j.at("aud").get_to( token.Aud );
-		if( j.find("email")!=j.end() )
-			j.at("email").get_to( token.Email );
-		if( j.find("email_verified")!=j.end() )
-			token.EmailVerified = j["email_verified"]=="true";
-		if( j.find("name")!=j.end() )
-			j.at("name").get_to( token.Name );
-		if( j.find("picture")!=j.end() )
-			j.at("picture").get_to( token.PictureUrl );
-		if( j.find("given_name")!=j.end() )
-			j.at("given_name").get_to( token.GivenName );
-		if( j.find("family_name")!=j.end() )
-			j.at("family_name").get_to( token.FamilyName );
-		if( j.find("locale")!=j.end() )
-			j.at("locale").get_to( token.Locale );
-		if( j.find("iat")!=j.end() )
-			token.Iat = j["iat"];
-		if( j.find("exp")!=j.end() )
-			token.Expiration = j["exp"];
-	}
-
-	#undef var
+	inline TokenInfo::TokenInfo( const jobject& j )ι:
+		Iss{ Json::FindDefaultSV(j, "iss") },
+		Azp{ Json::FindDefaultSV(j, "azp") },
+		Aud{ Json::FindDefaultSV(j, "aud") },
+		Email{ Json::FindDefaultSV(j, "email") },
+		EmailVerified{ Json::FindDefaultBool(j, "email_verified") },
+		Name{ Json::FindDefaultSV(j, "name") },
+		PictureUrl{ Json::FindDefaultSV(j, "picture") },
+		GivenName{ Json::FindDefaultSV(j, "given_name") },
+		FamilyName{ Json::FindDefaultSV(j, "family_name") },
+		Locale{ Json::FindDefaultSV(j, "locale") },
+		Iat{ Json::FindNumber<time_t>(j, "iat").value_or(0) },
+		Expiration{ Json::FindNumber<time_t>(j, "exp").value_or(0) }
+	{}
 }
